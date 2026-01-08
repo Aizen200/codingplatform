bee418b25380bbc611c32764b895e2ba6465ec42
Aizen200 <yatinbisht407@gmail.com>
2026-01-09 02:13:31 +0530

    important

---
+++ codingplatform/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Login from './pages/login'
+const App = () => {
+  return (
+    <div>
+      <Login/>
+    </div>
+  )
+}
+
+export default App
+
+
---
+++ codingplatform/src/pages/Login.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Login() {
+  return (
+    <div className="bg-gradient-to-br from-slate-950 via-slate-900 to-slate-950">
+      <div className='bg-black'><h1>Algorythm</h1>
+      <div>
+        <input type="email"  />
+        <br />
+        <input type="password" />
+      </div>
+      </div>
+
+    </div>
+  )
+}
