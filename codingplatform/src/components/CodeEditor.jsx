60e41170863e2905f36cc21a75f9a7f12b133381
Aizen200 <yatinbisht407@gmail.com>
2026-01-11 22:05:15 +0530

    finish styling of the codeeditor

--- codingplatform/src/components/CodeEditor.jsx
+++ codingplatform/src/components/CodeEditor.jsx
@@ -1,18 +1,72 @@
 import Editor from "@monaco-editor/react";
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 export default function CodeEditor() {
-  const [code, setCode] = useState("// type here");
+  const [input, setInput] = useState("");
+  const [output, setOutput] = useState("");
+  const coderef = useRef();
+
+  function mount(editor) {
+    coderef.current = editor;
+  }
+
+  function runbutton() {
+    const val = coderef.current.getValue();
+    setOutput(val + "\n\n" + input);
+  }
 
   return (
-    <div style={{ border: "1px solid #333" }}>
-      <Editor
-        height="400px"
-        language="javascript"
-        theme="vs-dark"
-        value={code}
-        onChange={(value) => setCode(value)}
-      />
+    <div className="min-h-screen bg-[#020617] text-gray-200 p-6 flex flex-col gap-6">
+
+     
+      <div className=" flex gap-4 flex-row-reverse">
+      <button className="px-5 py-2 bg-indigo-600 hover:bg-indigo-500 rounded-md text-sm font-semibold"> Submit</button>
+        <button
+          onClick={runbutton}
+          className="px-5 py-2 bg-indigo-600 hover:bg-indigo-500 rounded-md text-sm font-semibold"
+        >
+          Run
+        </button>
+      </div>
+
+     
+      <div className="border border-gray-700 rounded-md overflow-hidden">
+        <Editor
+          height="60vh"
+          language="python"
+          theme="vs-dark"
+          onMount={mount}
+        />
+      </div>
+
+
+      <div className="border border-gray-700 rounded-md">
+
+
+        <div className="flex border-b border-gray-700 text-sm">
+          <div className="px-4 py-2 text-indigo-400 border-b-2 border-indigo-500">
+            INPUT
+          </div>
+          <div className="px-4 py-2 text-gray-400">
+            OUTPUT
+          </div>
+        </div>
+
+
+        <textarea
+          placeholder="Enter input here..."
+          onChange={(e) => setInput(e.target.value)}
+          className="w-full h-32 bg-[#020617] text-gray-200 font-mono text-sm p-4 outline-none border-b border-gray-700 resize-none"
+        />
+
+        
+        <textarea
+          value={output}
+          readOnly
+          className="w-full h-32 bg-[#020617] text-gray-300 font-mono text-sm p-4 outline-none resize-none"
+        />
+      </div>
+
     </div>
   );
 }
